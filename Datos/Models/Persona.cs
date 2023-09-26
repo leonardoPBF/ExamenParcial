060fc092037a7f40782a4973fa544d52eb88b0a5
leonardoPBF <leonardobf140224rgmail.com>
2023-09-25 22:50:18 -0500

    1

---
+++ Datos/Models/Persona.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace practica.Models
+{
+    public class Persona
+    {
+        public int Dni {get; set;}
+        public string? Nombre {get; set;}
+        public string? Apellidos {get; set;}
+        
+
+
+    }
+}
